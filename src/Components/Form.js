8b96bebaa1d1cb6f97a15c29d6a5d781943f6f15
suriya1776 <suriyamuthukumar1776gmail.com>
2021-10-26 20:16:50 +0530

    submit function

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -1,98 +1,166 @@
 import React from 'react'
 import { useState } from 'react/cjs/react.development'
+
 import '../Styles/Form.css'
 
 function Form() {
-  const [studentData, setstudentData] = useState({})
-
-  const formdata = {
-    student: {
-      type: [
-        {
-          type: 'text',
-          value: 'Firstname',
-          classname: 'w-40 mx-2 p-3',
-          fieldvalue: '',
-        },
-        {
-          type: 'text',
-          value: 'Secondname',
-          classname: 'w-30 mx-2 p-3',
-          fieldvalue: 'Enter your Secondname',
-        },
-        {
-          type: 'text',
-          value: 'Lastname',
-          classname: 'w-30 mx-2 p-3',
-          fieldvalue: 'Enter your Lastname',
-        },
-
-        {
-          type: 'email',
-          value: 'Email',
-          classname: 'w-100 mx-2 p-3',
-          fieldvalue: 'Enter your email',
-        },
-
-        {
-          type: 'text',
-          value: 'Phonenumber',
-          classname: 'w-50 mx-2 p-3',
-          fieldvalue: 'Enter your phone number',
-        },
-        {
-          type: 'text',
-          value: 'Birthdate',
-          classname: 'w-50 mx-2 p-3',
-          fieldvalue: '',
-        },
-        {
-          type: 'text',
-          value: 'Ethnicity',
-          classname: 'w-50 mx-2 p-3',
-          fieldvalue: '',
-        },
-
-        {
-          type: 'text',
-          value: 'Gender',
-          classname: 'w-30 mx-2 p-3',
-          fieldvalue: '',
-        },
-
-        {
-          type: 'text',
-          value: 'StudentID',
-          classname: 'w-50 mx-2 p-3',
-          fieldvalue: 'ID',
-        },
-
-        {
-          type: 'text',
-          value: 'Semester',
-          classname: 'w-30 mx-2 p-3',
-          fieldvalue: '',
-        },
-      ],
+  const [studentData, setStudentData] = useState([
+    {
+      type: 'text',
+      value: 'Firstname',
+      classname: 'w-40 mx-2 p-3',
+      fieldvalue: 'Enter your firstname',
     },
+    {
+      type: 'text',
+      value: 'Secondname',
+      classname: 'w-30 mx-2 p-3',
+      fieldvalue: 'Enter your Secondname',
+    },
+    {
+      type: 'text',
+      value: 'Lastname',
+      classname: 'w-30 mx-2 p-3',
+      fieldvalue: 'Enter your Lastname',
+    },
+
+    {
+      type: 'email',
+      value: 'Email',
+      classname: 'w-100 mx-2 p-3',
+      fieldvalue: 'Enter your email',
+    },
+
+    {
+      type: 'text',
+      value: 'Phonenumber',
+      classname: 'w-50 mx-2 p-3',
+      fieldvalue: 'Enter your phone number',
+    },
+    {
+      type: 'text',
+      value: 'Birthdate',
+      classname: 'w-50 mx-2 p-3',
+      fieldvalue: '',
+    },
+    {
+      type: 'text',
+      value: 'Ethnicity',
+      classname: 'w-50 mx-2 p-3',
+      fieldvalue: '',
+    },
+
+    {
+      type: 'text',
+      value: 'Gender',
+      classname: 'w-30 mx-2 p-3',
+      fieldvalue: '',
+    },
+
+    {
+      type: 'text',
+      value: 'StudentID',
+      classname: 'w-100 mx-2 p-3',
+      fieldvalue: 'ID',
+    },
+
+    {
+      type: 'text',
+      value: 'Semester',
+      classname: 'w-100 mx-2 p-3 below_form',
+      fieldvalue: '',
+    },
+  ])
+
+  const ChangeHandler = (e, index) => {
+    const values = [...studentData]
+    values[index].fieldvalue = e.target.value
+    setStudentData(values)
+    console.log(studentData)
+  }
+  const AddHandler = () => {
+    setStudentData([
+      ...studentData,
+      {
+        type: 'text',
+        value: 'Firstname',
+        classname: 'w-40 mx-2 p-3',
+        fieldvalue: 'Enter your firstname',
+      },
+      {
+        type: 'text',
+        value: 'Secondname',
+        classname: 'w-30 mx-2 p-3',
+        fieldvalue: 'Enter your Secondname',
+      },
+      {
+        type: 'text',
+        value: 'Lastname',
+        classname: 'w-30 mx-2 p-3',
+        fieldvalue: 'Enter your Lastname',
+      },
+
+      {
+        type: 'email',
+        value: 'Email',
+        classname: 'w-100 mx-2 p-3',
+        fieldvalue: 'Enter your email',
+      },
+
+      {
+        type: 'text',
+        value: 'Phonenumber',
+        classname: 'w-50 mx-2 p-3',
+        fieldvalue: 'Enter your phone number',
+      },
+      {
+        type: 'text',
+        value: 'Birthdate',
+        classname: 'w-50 mx-2 p-3',
+        fieldvalue: '',
+      },
+      {
+        type: 'text',
+        value: 'Ethnicity',
+        classname: 'w-50 mx-2 p-3',
+        fieldvalue: '',
+      },
+
+      {
+        type: 'text',
+        value: 'Gender',
+        classname: 'w-30 mx-2 p-3',
+        fieldvalue: '',
+      },
+
+      {
+        type: 'text',
+        value: 'StudentID',
+        classname: 'w-100 mx-2 p-3',
+        fieldvalue: 'ID',
+      },
+
+      {
+        type: 'text',
+        value: 'Semester',
+        classname: 'w-100 mx-2 p-3 below_form',
+        fieldvalue: '',
+      },
+    ])
   }
 
-  const ChangeHandler = (e, index, id) => {
-    let Initial_value = student.type
-    setstudentData(Initial_value)
-    if (id === 'Firstname') {
-      setstudentData((student.type[index].fieldvalue = e.target.value))
-    }
+  const submitHandler = (e) => {
+    e.preventDefault()
+    console.log(studentData)
   }
-
-  const { student } = formdata
 
   return (
     <div className='row g-3'>
       <div className='w-100'>
         <h1 className='form_heading'>STUDENT DATA</h1>
       </div>
-      {student.type.map((type, sIndex) => {
+
+      {studentData.map((type, sIndex) => {
         return (
           <div className={type.classname}>
             <label htmlFor={type.value} className='form-label'>
@@ -102,18 +170,25 @@ function Form() {
               type={type.type}
               className='form-control'
               id={type.value}
-              value={studentData.type[sIndex].fieldvalue}
-              onChange={(e, id) => ChangeHandler(e, sIndex, id)}
+              value={type.fieldvalue}
+              onChange={(e) => ChangeHandler(e, sIndex)}
             ></input>
           </div>
         )
       })}
-
       <div className='w-100'>
-        <button type='button' className='btn btn-primary '>
+        <button
+          type='button'
+          className='btn btn-primary '
+          onClick={() => AddHandler()}
+        >
           Add Person
         </button>
-        <button type='button' className='btn btn-success'>
+        <button
+          type='button'
+          className='btn btn-success '
+          onClick={(e) => submitHandler(e)}
+        >
           Submit
         </button>
       </div>
