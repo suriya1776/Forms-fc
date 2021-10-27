52a54b61a1a1f81c49ed819d34ee0ce87a522a6f
suriya1776 <suriyamuthukumar1776gmail.com>
2021-10-27 16:00:34 +0530

    fetching from database

--- src/Pages/Database.js
+++ src/Pages/Database.js
@@ -1,6 +1,23 @@
 import React from 'react'
+import { useState, useEffect } from 'react'
+import api from '../Axios/Axios'
 
 function Database() {
+  const [data, setData] = useState([])
+
+  const url = '/studentsdata'
+
+  useEffect(() => {
+    api
+      .get(url)
+      .then((res) => {
+        setData(res.data)
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [url])
+  console.log(data)
   return (
     <div>
       <h1>database page</h1>
